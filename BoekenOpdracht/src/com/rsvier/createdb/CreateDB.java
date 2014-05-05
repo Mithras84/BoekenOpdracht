5ee7a6d402eb924df8723d002a6d9a5b58938063
Pieter <p.c.zeeuw@gmail.com>
2014-05-05 12:44:35 +0200

    Create 3 tables: Voorraad, Boek and Rekeningen

--- BoekenOpdracht/src/com/rsvier/createdb/CreateDB.java
+++ BoekenOpdracht/src/com/rsvier/createdb/CreateDB.java
@@ -5,6 +5,7 @@ package com.rsvier.createdb;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Connection;
+import java.sql.Statement;
 
 /**
  * Class description
@@ -24,10 +25,10 @@ import java.sql.Connection;
 public class CreateDB {
     protected  Connection mDBConnection;
     
-    public void initConnection() {
+    public void initConnection() throws SQLException {
 	// TODO Auto-generated method stub
 	String host = "jdbc:mysql://127.0.0.1:3306/";
-	String dbName = "boeken_opdracht";
+	String dbName = "winkel";
 	String userName = "user=root";
 	String passWord = "password=";
 	
@@ -38,17 +39,58 @@ public class CreateDB {
 	}	
     }
     
-    protected boolean createConnection (String url) {
-	try {
-	    //Class.forName("com.oracle.jdbc.Driver");
-	    mDBConnection = DriverManager.getConnection(url);
-	} catch (SQLException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	    return false;
-	}
-		
-	return true;
+    public boolean closeConnection() throws SQLException {
+	mDBConnection.close();
+	return mDBConnection.isClosed();
+    }
+    
+    public boolean createTableVoorraad () throws SQLException {
+	String query = "CREATE TABLE IF NOT EXISTS voorraad "
+		+ "("
+		+ "isbn INT NOT NULL PRIMARY KEY,"
+		+ "amsterdam INT,"
+		+ "groningen INT,"
+		+ "utrecht INT"
+		+ ")";
+	
+	Statement stmt = mDBConnection.createStatement();
+	return stmt.execute(query);
+    }
+    
+    public boolean createTableBoek () throws SQLException {
+	String query = "CREATE TABLE IF NOT EXISTS boek "
+		+ "("
+		+ "isbn INT NOT NULL PRIMARY KEY,"
+		+ "titel VARCHAR(255),"
+		+ "auteur VARCHAR(255),"
+		+ "prijs INT,"
+		+ "genre VARCHAR(255),"
+		+ "paginas INT"
+		+ ")";
+	
+	Statement stmt = mDBConnection.createStatement();
+	return stmt.execute(query);
+    }
+    
+    public boolean createTableRekeningen () throws SQLException {
+	String query = "CREATE TABLE IF NOT EXISTS rekeningen "
+		+ "("
+		+ "rekeningnummer INT NOT NULL PRIMARY KEY,"
+		+ "naam VARCHAR(255),"
+		+ "plaats VARCHAR(255),"
+		+ "saldo CURRENCY"
+		+ ")";
+	
+	Statement stmt = mDBConnection.createStatement();
+	return stmt.execute(query);
+    }
+    
+    protected boolean createConnection (String url) throws SQLException { 
+	mDBConnection = DriverManager.getConnection(url);
+	
+	if (mDBConnection.isValid(0))
+	    return true;
+	else return false;
     }
     
     
--- BoekenOpdracht/src/com/rsvier/createdb/Start.java
+++ BoekenOpdracht/src/com/rsvier/createdb/Start.java
@@ -3,6 +3,8 @@
  */
 package com.rsvier.createdb;
 
+import java.sql.SQLException;
+
 /**
  * Class description
  * 
@@ -10,15 +12,39 @@ package com.rsvier.createdb;
  * @author 		Pieter
  */
 public class Start {
+    static CreateDB mCdb;
 
     /**
      * @param args
      */
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
-	CreateDB cdb = new CreateDB();
-	cdb.initConnection();
-
+	CreateDB mCdb = new CreateDB();
+	try {
+	    //Open connection.
+	    mCdb.initConnection();
+	    
+	    //Create all tables:
+	    createTables();
+	    
+	    //Close connection.
+	    if (mCdb.closeConnection() )
+		System.out.println("Connection closed!");
+	    
+	} catch (SQLException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	} 
+    }
+    
+    protected static void createTables () throws SQLException {
+	//Create table Voorraad:
+	if (mCdb.createTableVoorraad() )
+	    System.out.println("Table 'Voorraad' Created");
+	if (mCdb.createTableBoek() )
+	    System.out.println("Table 'Boek' Created");
+	if (mCdb.createTableRekeningen() ) 
+	    System.out.println("Table 'Rekeningen' Created");
     }
 
 }
