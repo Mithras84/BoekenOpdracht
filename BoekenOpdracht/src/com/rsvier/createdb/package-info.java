1b6e834d9d29440b90b8603566b4cc410e23852b
Pieter <p.c.zeeuw@gmail.com>
2014-05-05 10:53:45 +0200

    Bezig met programma om een database aan te maken.

---
+++ BoekenOpdracht/src/com/rsvier/createdb/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package description:
+ * Simpel programma om een nieuwe database met tabellen aan te maken en deze te vullen met boeken en rekeningen.
+ * Onderdeel van Remi's boekenopdracht voor RSVier.
+ * 
+ * @version		1.00 5 mei 2014
+ * @author 		Pieter
+ */
+package com.rsvier.createdb;
