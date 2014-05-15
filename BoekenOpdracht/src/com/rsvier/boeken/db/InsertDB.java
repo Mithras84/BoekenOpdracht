5399bb9ceb7454b0d3d6b5b9e443b9975d8a673b
Pieter <p.c.zeeuw@gmail.com>
2014-05-15 15:47:41 +0200

    Unused import removed

--- BoekenOpdracht/src/com/rsvier/boeken/db/InsertDB.java
+++ BoekenOpdracht/src/com/rsvier/boeken/db/InsertDB.java
@@ -5,7 +5,6 @@ package com.rsvier.boeken.db;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.rsvier.boeken.model.Boek;
